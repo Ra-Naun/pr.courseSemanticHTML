f557901ffa86ff7c3e49fa2f422b5e656b16a812
Ra-Naun <dmitriy.slavskiy@hotger.com>
2022-12-08 15:40:07 +0100

    minor changes

--- app/src/components/common/Layout/Logo/Logo.tsx
+++ app/src/components/common/Layout/Logo/Logo.tsx
@@ -5,12 +5,12 @@ import styles from './Logo.module.scss';
 
 const Logo: FC = () => {
   return (
-    <div className={styles['logo-wrapper']}>
+    <a href="/" className={styles['logo-wrapper']}>
       <LogoImg className={styles['logo']} />
       <span className={styles['logo-title']}>
         PANTHEON <p className={styles['logo-subtitle']}>project</p>
       </span>
-    </div>
+    </a>
   );
 };
 
